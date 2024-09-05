02191ea65ef8b317b946597f565055a64ae11497
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-05 16:33:50 +0000

    openapi-generation@83f0261fa4066041bfd5c85c934d65c647269eea

--- internal/provider/resource_data_source.go
+++ internal/provider/resource_data_source.go
@@ -451,7 +451,7 @@ func (r *ResourceDataSource) Schema(ctx context.Context, req datasource.SchemaRe
 								Attributes: map[string]schema.Attribute{
 									"operator": schema.StringAttribute{
 										Computed:    true,
-										Description: `The operator of the reviewer stage. Admin and manager approval are also treated as reviewers. must be one of ["AND", "OR"]`,
+										Description: `The operator of the reviewer stage. Admin and manager approval are also treated as reviewers.`,
 									},
 									"owner_ids": schema.SetAttribute{
 										Computed:    true,
@@ -483,7 +483,7 @@ func (r *ResourceDataSource) Schema(ctx context.Context, req datasource.SchemaRe
 			},
 			"resource_type": schema.StringAttribute{
 				Computed:    true,
-				Description: `The type of the resource. must be one of ["AWS_IAM_ROLE", "AWS_EC2_INSTANCE", "AWS_EKS_CLUSTER", "AWS_RDS_POSTGRES_INSTANCE", "AWS_RDS_MYSQL_INSTANCE", "AWS_ACCOUNT", "AWS_SSO_PERMISSION_SET", "CUSTOM", "GCP_BUCKET", "GCP_COMPUTE_INSTANCE", "GCP_FOLDER", "GCP_GKE_CLUSTER", "GCP_PROJECT", "GCP_CLOUD_SQL_POSTGRES_INSTANCE", "GCP_CLOUD_SQL_MYSQL_INSTANCE", "GIT_HUB_REPO", "GIT_LAB_PROJECT", "GOOGLE_WORKSPACE_ROLE", "MONGO_INSTANCE", "MONGO_ATLAS_INSTANCE", "OKTA_APP", "OKTA_ROLE", "OPAL_ROLE", "PAGERDUTY_ROLE", "TAILSCALE_SSH", "SALESFORCE_PERMISSION_SET", "SALESFORCE_PROFILE", "SALESFORCE_ROLE", "WORKDAY_ROLE", "MYSQL_INSTANCE", "MARIADB_INSTANCE", "TELEPORT_ROLE"]`,
+				Description: `The type of the resource.`,
 			},
 			"ticket_propagation": schema.SingleNestedAttribute{
 				Computed: true,
@@ -499,7 +499,7 @@ func (r *ResourceDataSource) Schema(ctx context.Context, req datasource.SchemaRe
 					},
 					"ticket_provider": schema.StringAttribute{
 						Computed:    true,
-						Description: `The third party ticketing platform provider. must be one of ["JIRA", "LINEAR", "SERVICE_NOW"]`,
+						Description: `The third party ticketing platform provider.`,
 					},
 				},
 				Description: `Configuration for ticket propagation, when enabled, a ticket will be created for access changes related to the users in this resource.`,
